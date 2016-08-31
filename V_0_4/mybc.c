4a1c225dad950723379a94cf88abadd0442c2d3b
williamburghesi <williamburghesi@gmail.com>
2016-08-31 02:38:20 -0300

    Add files via upload

---
+++ V_0_4/mybc.c
@@ -0,0 +1,43 @@
+/**@<mybc.c>::**/
+
+/*
+1: Tue Aug 16 20:50:47 BRT 2016
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+FILE *source;
+
+extern int lookahead; // @ parser.c
+extern int checa_parenteses = 0;
+
+main (int argc, char *argv[], char *envp[])
+{
+        if (argc == 1) {
+                source = stdin;
+        } else {
+                source = fopen (argv[1], "r");
+                if (source == NULL) {
+                        fprintf (stderr, "%s: cannot open %s... exiting\n",
+                                argv[0], argv[1]);
+                        exit (-1);
+                }
+        }
+
+        lookahead = gettoken (source);
+
+        expr ();
+
+	match(EOF);
+	printf("lookahead:%d fora\n",lookahead);
+	if(lookahead==EOF)
+	{
+	printf("dentro\n");
+		if (checa_parenteses!=0)
+		{
+		printf("Erro!\n");
+		}	
+	}
+        exit (0);
+}
---
+++ V_0_4/parser.c
@@ -0,0 +1,123 @@
+/**@<parser.c>::**/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <tokens.h>
+#include <parser.h>
+
+int checa_parenteses;
+/*************************** LL(1) grammar definition ******************************
+ *
+ * expr -> term {addop term}
+ *
+ * term -> fact {mulop fact}
+ *
+ * fact -> vrbl | cons | ( expr )
+ *
+ * vrbl -> ID
+ *
+ * cons -> DEC
+ *
+ * addop -> '+' | '-'
+ *
+ * mulop -> '*' | '/'
+ *
+ * ID = [A-Za-z][A-Za-z0-9]*
+ *
+ * DEC = [1-9][0-9]* | 0
+ *
+ **********************************************************************************/
+
+/***************************** LL(1) grammar emulation *****************************
+ *
+ * expr -> term { addop term } */
+
+
+void expr (void)
+{
+	E_entry:
+	T_entry:
+	F_entry:
+		switch (lookahead) 
+		{
+		case ID:
+			match (ID); 
+			break;
+		case DEC:
+			match (DEC); 
+			break;
+		default:
+			if(lookahead=='(')
+			{
+				match ('('); 
+				checa_parenteses++;
+			 	goto E_entry;
+			}
+			else 
+			{ 	printf("look:%d",lookahead);
+				match (')'); 
+ 				checa_parenteses--; 
+			}
+			break;
+		}
+		if(mulop()) 
+			goto F_entry;
+		if(addop()) 
+			goto T_entry;
+ 	
+	
+	printf("checa %d\n",checa_parenteses);
+
+}
+
+
+/*
+ * vrbl -> ID
+ *
+ * cons -> DEC
+*
+ * ID = [A-Za-z][A-Za-z0-9]*
+ *
+ * DEC = [1-9][0-9]* | 0
+ *
+ * addop -> '+' | '-' */
+int addop (void)
+{
+	switch(lookahead){
+	case '+':
+			match('+'); return '+';
+	case '-':
+			match('-'); return '-';
+	}
+	return 0;
+}
+
+/*
+ * mulop -> '*' | '/' */
+int mulop (void)
+{
+	switch(lookahead){
+	case '*':
+			match('*'); return '*';
+	case '/':
+			match('/'); return '/';
+	}
+	return 0;
+}
+/***************************** lexer-to-parser interface **************************/
+
+int lookahead; // @ local
+
+void match (int expected)
+ {
+	 if ( expected == lookahead) {
+		 lookahead = gettoken (source);
+	 } else {
+		 fprintf(stderr,"parser: token mismatch error. found # %d ",
+		 	lookahead);
+		 fprintf(stderr,"whereas expected # %d\n",
+		 	expected);
+		 exit (SYNTAX_ERR);
+	 }
+ }
+
